8d2db490b85bef7657cd501856225501b548c5c7
sulis setyawati <sulis.setyawati.w@gmail.com>
2019-01-16 16:21:57 +0700

    ftygfy

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/* Favorit! program in C#.*/
+using System;
+namespace Fav
+{
+    class Favo
+    {
+        static void Main()
+        {
+            Console.WriteLine("5 makanan favorit");
+            Console.WriteLine("nasi,kacang hijau,ikan,jamur,mie ayam");
+            Console.WriteLine("5 tempat kenangan");
+            Console.WriteLine("Madin, Masjid At Taqwa, Masjid MIPA UGM, Ditmawa, Shofuro");
+            Console.WriteLine("5 Orang tercinta");
+            Console.WriteLine("Ibuk, Bapak, kakak pertama, kakak kedua, aku");
+            Console.WriteLine("5 Buku favorit");
+            Console.WriteLine("Semua Ada Saatnya, Lapis Lapis Keberkahan, Khadijah, Saksikan bahwa Aku seorang Muslim, Jalan Cinta Para Pejuang");
+            Console.WriteLine("5 Film Favorit");
+            Console.WriteLine("Tengkorak, Detective Conan, Transformers, Doraemon, toy story");
+            Console.WriteLine();
+       
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
