66a23157016a5f8aaab5a3b37e7263586d75d44b
sulis setyawati <sulis.setyawati.w@gmail.com>
2019-01-16 16:27:46 +0700

    gdj

---
+++ exam02/soal4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam02
+{
+    class soal4
+    {
+        public soal4()
+        {
+
+        }
+        private void (CetakPola
+    }
+}
